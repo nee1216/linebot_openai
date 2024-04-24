9cf968259da78a09f490b07482959ffef47ce0a0
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-04-24 16:24:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,30 +34,6 @@ def GPT_response(text):
     answer = response['choices'][0]['text'].replace('。','')
     return answer
 
-def linebot():
-    body = request.get_data(as_text=True)                    # 取得收到的訊息內容
-    try:
-        json_data = json.loads(body)                         # json 格式化訊息內容
-        access_token = 'tsGykdGQN1KnwwQWwkkmq7JM0ji0RnYXFa0DBN3sfLVJ4wgcXudGmWpUZst3ZDBHXCL7xp2NhVrR1eDJKdExozjb6DInsSdHeSw1rtrjmz9Bi3Tx/YiI1g4/yGU95a0Jg15MyGM9QFCNdrM2SfU+XQdB04t89/1O/w1cDnyilFU='
-        secret = '0584d0fc476d78024afcd7cbbf8096b4'
-        line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
-        handler = WebhookHandler(secret)                     # 確認 secret 是否正確
-        signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
-        handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
-        tk = json_data['events'][0]['replyToken']            # 取得回傳訊息的 Token
-        type = json_data['events'][0]['message']['type']     # 取得 LINe 收到的訊息類型
-        if type=='text':
-            msg = json_data['events'][0]['message']['text']  # 取得 LINE 收到的文字訊息
-            print(msg)                                       # 印出內容
-            reply = msg
-        else:
-            reply = '你傳的不是文字呦～'
-        print(reply)
-        line_bot_api.reply_message(tk,TextSendMessage(reply))# 回傳訊息
-    except:
-        print(body)                                          # 如果發生錯誤，印出收到的內容
-    return 'OK'                    
-
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
