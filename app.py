38a05a955d83f40a3f69c57cc3bc28775d946e5a
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-04-24 17:42:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,6 @@ import datetime
 import openai
 import time
 import traceback
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
 #======python的函數庫==========
 
 app = Flask(__name__)
@@ -28,37 +25,6 @@ handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 # OPENAI API Key初始化設定
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-def get_transit_info():
-    options = webdriver.ChromeOptions()
-    service = ChromeService(executable_path="chromedriver.exe")
-    driver = webdriver.Chrome(service=service, options=options)
-
-    try:
-        driver.get("https://transit.navitime.com/zh-tw/tw/transfer?start=00016389&goal=00022583") 
-        driver.maximize_window()
-        driver.refresh()
-        time.sleep(3) 
-
-        # 獲取交通資訊
-        table_element = driver.find_element(By.ID, "transit-1")
-        transit_info_1 = "捷運士林站(中正)-東吳大學:\n" + table_element.text
-
-        table_element = driver.find_element(By.ID, "transit-2")
-        transit_info_2 = "\n\n" + table_element.text
-
-        transit_info = transit_info_1 + transit_info_2
-        print(transit_info)
-
-        return transit_info
-
-    except KeyboardInterrupt:
-        print("程式被用戶中斷.")
-        return None
-    except Exception as e:
-        print("發生錯誤:", str(e))
-        return None
-    finally:
-        driver.close()
 
 def GPT_response(text):
     # 接收回應
@@ -67,6 +33,7 @@ def GPT_response(text):
     # 重組回應
     answer = response['choices'][0]['text'].replace('。','')
     return answer
+
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -83,23 +50,24 @@ def callback():
         abort(400)
     return 'OK'
 
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    
-    if msg == '交通資訊':
-        # 呼叫函數獲取交通資訊
-        transit_info = get_transit_info()
-        if transit_info:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(transit_info))
-        else:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage('無法獲取交通資訊'))
-
+    try:
+        GPT_answer = GPT_response(msg)
+        print(GPT_answer)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
+    except:
+        print(traceback.format_exc())
+        line_bot_api.reply_message(event.reply_token, TextSendMessage('你好啊'))
+        
 
 @handler.add(PostbackEvent)
-def handle_postback(event):
+def handle_message(event):
     print(event.postback.data)
+
 
 @handler.add(MemberJoinedEvent)
 def welcome(event):
@@ -110,6 +78,9 @@ def welcome(event):
     message = TextSendMessage(text=f'{name}歡迎加入')
     line_bot_api.reply_message(event.reply_token, message)
 
+
+        
+        
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
