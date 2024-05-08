524ef9edc337fd80b972695ec57e6c0fe3dc1aad
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-05-08 16:26:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,19 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 import time
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
-import chromedriver.exe from
+import os
 
-# LINE Bot 的設定
-line_bot_api = LineBotApi('tsGykdGQN1KnwwQWwkkmq7JM0ji0RnYXFa0DBN3sfLVJ4wgcXudGmWpUZst3ZDBHXCL7xp2NhVrR1eDJKdExozjb6DInsSdHeSw1rtrjmz9Bi3Tx/YiI1g4/yGU95a0Jg15MyGM9QFCNdrM2SfU+XQdB04t89/1O/w1cDnyilFU=')
-user_id = 'U0a15453a9b9531b8a1517ce2863fbc84'  # 對象使用者的ID
+# 使用環境變量或其他配置方式存儲敏感信息
+line_bot_api = LineBotApi(os.getenv('tsGykdGQN1KnwwQWwkkmq7JM0ji0RnYXFa0DBN3sfLVJ4wgcXudGmWpUZst3ZDBHXCL7xp2NhVrR1eDJKdExozjb6DInsSdHeSw1rtrjmz9Bi3Tx/YiI1g4/yGU95a0Jg15MyGM9QFCNdrM2SfU+XQdB04t89/1O/w1cDnyilFU='))
+user_id = os.getenv('U0a15453a9b9531b8a1517ce2863fbc84')
 
+# 設置無頭瀏覽器
 options = webdriver.ChromeOptions()
-service = ChromeService(executable_path="chromedriver.exe")
+options.add_argument('--headless')
+
+# 使用相對路徑
+chromedriver_path = './chromedriver.exe'  # '.' 表示當前工作目錄
+service = ChromeService(executable_path=chromedriver_path)
 driver = webdriver.Chrome(service=service, options=options)
 
 while True:
@@ -31,10 +36,14 @@ while True:
         # 傳送消息
         for message in messages:
             line_bot_api.push_message(user_id, message)
-    
+        
+        # 控制爬蟲和發送頻率
+        time.sleep(3600)  # 等待一小時再繼續執行
+        
     except Exception as e:
         print("發生錯誤:", str(e))
     
     print("---------------------------------------------------------------------------------------")
 
-driver.close()
+driver.quit()
+
