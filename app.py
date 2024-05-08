92063ca6b40cfc2121a223b66f02164a920609c0
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-05-08 17:20:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # LINE Bot 的 Channel Access Token 和 Channel Secret
 line_bot_api = LineBotApi("tsGykdGQN1KnwwQWwkkmq7JM0ji0RnYXFa0DBN3sfLVJ4wgcXudGmWpUZst3ZDBHXCL7xp2NhVrR1eDJKdExozjb6DInsSdHeSw1rtrjmz9Bi3Tx/YiI1g4/yGU95a0Jg15MyGM9QFCNdrM2SfU+XQdB04t89/1O/w1cDnyilFU=")
-handler = WebhookHandler("0584d0fc476d78024afcd7cbbf8096b4)
+handler = WebhookHandler("0584d0fc476d78024afcd7cbbf8096b4")
 
 @app.route("/callback", methods=['POST'])
 def callback():
