cc1b3a0a6e72517508ab2bada91855171793d194
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-05-15 16:56:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 import time
+import requests
 
 app = Flask(__name__)
 
 # LINE Bot 的 Channel Access Token 和 Channel Secret
 LINE_CHANNEL_ACCESS_TOKEN = "tsGykdGQN1KnwwQWwkkmq7JM0ji0RnYXFa0DBN3sfLVJ4wgcXudGmWpUZst3ZDBHXCL7xp2NhVrR1eDJKdExozjb6DInsSdHeSw1rtrjmz9Bi3Tx/YiI1g4/yGU95a0Jg15MyGM9QFCNdrM2SfU+XQdB04t89/1O/w1cDnyilFU="
 LINE_CHANNEL_SECRET = "0584d0fc476d78024afcd7cbbf8096b4"
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -26,12 +30,25 @@ def callback():
 
     return 'OK'
 
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+chrome_driver_url = "https://raw.githubusercontent.com/nee1216/linebot_openai/master/chromedriver.exe"
+driver_path = "chromedriver.exe"
+
+response = requests.get(chrome_driver_url)
+with open(driver_path, 'wb') as f:
+    f.write(response.content)
+
+executable_path = driver_path
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text == "最近消息":
         try:
             options = webdriver.ChromeOptions()
-            service = ChromeService(executable_path="https://raw.githubusercontent.com/nee1216/linebot_openai/master/chromedriver.exe")
+            service = ChromeService(executable_path=executable_path)
             driver = webdriver.Chrome(service=service, options=options)
 
             response = ""
