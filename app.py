617cb37364f608ef2e65fb668ee674e0d0d02c07
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-05-16 16:47:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 # LINE Bot 的 Channel Access Token 和 Channel Secret
-LINE_CHANNEL_ACCESS_TOKEN = "tsGykdGQN1KnwwQWwkkmq7JM0ji0RnYXFa0DBN3sfLVJ4wgcXudGmWpUZst3ZDBHXCL7xp2NhVrR1eDJKdExozjb6DInsSdHeSw1rtrjmz9Bi3Tx/YiI1g4/yGU95a0Jg15MyGM9QFCNdrM2SfU+XQdB04t89/1O/w1cDnyilFU="
-LINE_CHANNEL_SECRET = "0584d0fc476d78024afcd7cbbf8096b4"
+LINE_CHANNEL_ACCESS_TOKEN = "YOUR_CHANNEL_ACCESS_TOKEN"
+LINE_CHANNEL_SECRET = "YOUR_CHANNEL_SECRET"
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-# 创建字典来追踪用户的科系选择
+# 创建字典来追踪用户的选择
 user_choices = {}
 
 @app.route("/callback", methods=['POST'])
@@ -31,49 +31,154 @@ def callback():
     
     return 'OK'
 
-# 當收到 LINE 消息时的回调函数
+def send_transit_info(event, user_message):
+    # 发送公车路线信息给用户
+    if user_message == "士林捷運站-東吳大學":
+        url = "URL_1"
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            transit_element = soup.find(id="transit-1")
+            if transit_element:
+                time_element = transit_element.find(class_="time display-inline text-frame")
+                if time_element:
+                    time_text = time_element.get_text(strip=True)
+                    transit_info = "捷運士林站(中正)-東吳大學:(557)\n" + time_text
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=transit_info))
+                else:
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="未找到指定的 class 元素。"))
+            else:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text="未找到指定的 id 元素。"))
+        else:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"无法获取页面内容。状态码: {response.status_code}"))
+    elif user_message == "東吳大學-士林捷運站":
+        # 处理其他路线信息的逻辑，以及其他路线的请求和回复
+        pass
+    # 处理其他公车路线的逻辑，以及其他路线的请求和回复
+
+# 当收到 LINE 消息时的回调函数
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_message = event.message.text
     user_id = event.source.user_id  # 获取用户的 ID
     
-    # 判断用户是否选择了交通路线
+    # 判断是否是用户点击科系简介
     if user_message == "交通":  
-        # 发送快速回复，让用户选择公车路线
-        quick_reply = QuickReply(items=[
-            QuickReplyButton(action=MessageAction(label="士林捷運站-東吳大學", text="士林捷運站-東吳大學")),
-            QuickReplyButton(action=MessageAction(label="東吳大學-士林捷運站", text="東吳大學-士林捷運站")),
-            QuickReplyButton(action=MessageAction(label="士林捷運站-東吳大學(錢穆故居)", text="士林捷運站-東吳大學(錢穆故居)")),
-            QuickReplyButton(action=MessageAction(label="東吳大學(錢穆故居)-士林捷運站", text="東吳大學(錢穆故居)-士林捷運站")),
-            QuickReplyButton(action=MessageAction(label="捷運劍南路站-東吳大學(錢穆故居)", text="捷運劍南路站-東吳大學(錢穆故居)"))
-        ])
+        # 构建 Flex Message
+        flex_message = {
+            "type": "bubble",
+            "header": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "选择想了解的科系",
+                        "align": "center",
+                        "weight": "bold",
+                        "size": "xl",
+                        "color": "#ffffff"
+                    }
+                ],
+                "backgroundColor": "#471B00"
+            },
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "🌟先选择想了解的科系之后，就可以查看该系的必选修课程资讯咯!!!!",
+                        "size": "md",
+                        "wrap": True,
+                        "weight": "bold"
+                    }
+                ]
+            },
+            "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "button",
+                        "style": "primary",
+                        "color": "#905c44",
+                        "action": {
+                            "type": "message",
+                            "label": "資料科學系",
+                            "text": "資料科學系"
+                        }
+                    },
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "margin": "sm"
+                    },
+                    {
+                        "type": "button",
+                        "style": "primary",
+                        "color": "#905c44",
+                        "action": {
+                            "type": "message",
+                            "label": "資料管理系",
+                            "text": "資料管理系"
+                        }
+                    },
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "margin": "sm"
+                    },
+                    {
+                        "type": "button",
+                        "style": "primary",
+                        "color": "#905c44",
+                        "action": {
+                            "type": "message",
+                            "label": "法律系",
+                            "text": "法律法律系"
+                        }
+                    },
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "margin": "sm"
+                    },
+                    {
+                        "type": "button",
+                        "style": "primary",
+                        "color": "#905c44",
+                        "action": {
+                            "type": "message",
+                            "label": "化學系",
+                            "text": "化學系"
+                        }
+                    },
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "margin": "sm"
+                    },
+                    {
+                        "type": "button",
+                        "style": "primary",
+                        "color": "#905c44",
+                        "action": {
+                            "type": "message",
+                            "label": "日文系",
+                            "text": "日文系"
+                        }
+                    }
+                ]
+            }
+        }
         
-        # 发送快速回复给用户
-        reply_text = TextSendMessage(text="請選擇公車路線?", quick_reply=quick_reply)
-        line_bot_api.reply_message(event.reply_token, reply_text)
+        # 发送 Flex Message 给用户
+        line_bot_api.reply_message(
+            event.reply_token,
+            FlexSendMessage(alt_text="选择想了解的科系", contents=flex_message)
+        )
         
     # 判断用户是否选择了公车路线
     elif user_message in ["士林捷運站-東吳大學", "東吳大學-士林捷運站", "士林捷運站-東吳大學(錢穆故居)", "東吳大學(錢穆故居)-士林捷運站", "捷運劍南路站-東吳大學(錢穆故居)"]:
-        # 记录用户选择的路线
-        user_choices[user_id] = user_message
-        
-        # 发送相应的结果给用户
-        if user_message == "士林捷運站-東吳大學":
-            send_transit_info(event, user_message)
-        elif user_message == "東吳大學-士林捷運站":
-            send_transit_info(event, user_message)
-        elif user_message == "士林捷運站-東吳大學(錢穆故居)":
-            send_transit_info(event, user_message)
-        elif user_message == "東吳大學(錢穆故居)-士林捷運站":
-            send_transit_info(event, user_message)
-        elif user_message == "捷運劍南路站-東吳大學(錢穆故居)":
-            send_transit_info(event, user_message)
-            
-    else:
-        # 发送默认回复
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="請輸入正確的命令。")
-        )
-
-def send_transit
+        send_transit_info(event, user_message)  # 调用
