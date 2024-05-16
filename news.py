1006172ba9a559cc2573bf90af506c5fcec3d721
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-05-16 13:37:23 +0800

    Update news.py

--- news.py
+++ news.py
@@ -1,27 +1,58 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-import time
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import requests
+from bs4 import BeautifulSoup
 
-options = webdriver.ChromeOptions()
-service = ChromeService(executable_path="chromedriver.exe")
-driver = webdriver.Chrome(service=service, options=options)
+app = Flask(__name__)
 
-while True:
+# LINE Bot 的 Channel Access Token 和 Channel Secret
+LINE_CHANNEL_ACCESS_TOKEN = "tsGykdGQN1KnwwQWwkkmq7JM0ji0RnYXFa0DBN3sfLVJ4wgcXudGmWpUZst3ZDBHXCL7xp2NhVrR1eDJKdExozjb6DInsSdHeSw1rtrjmz9Bi3Tx/YiI1g4/yGU95a0Jg15MyGM9QFCNdrM2SfU+XQdB04t89/1O/w1cDnyilFU="
+LINE_CHANNEL_SECRET = "0584d0fc476d78024afcd7cbbf8096b4"
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
     try:
-        driver.get("https://www-news.scu.edu.tw/news-7?page=1")
-        time.sleep(5)
-        tbody = driver.find_element(By.XPATH, "//tbody")
-        
-        links = tbody.find_elements(By.TAG_NAME, "a")
-        
-        for link in links:
-            print("校園頭條:", link.text)
-            print("連結:", link.get_attribute("href"))
-            
-    except Exception as e:
-            print("發生錯誤:", str(e))
-        
-    print("---------------------------------------------------------------------------------------")
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-driver.close()
+    return 'OK'
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.message.text == "最新消息":
+        news_message = latest_news()
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=news_message))
+
+def latest_news():
+    try:
+        message = ""
+        response = requests.get("https://www-news.scu.edu.tw/news-7?page=1")
+        root = BeautifulSoup(response.text, "html.parser")
+        tbody = root.find("tbody")
+        links = tbody.find_all("a")
+
+        for link in links:
+            message += "校園頭條:\n{}\n".format(link.text.strip())
+            message += "連結: {}\n\n".format(link["href"])
+
+        return message.strip() 
+    
+    except Exception as e:
+        return '無法取得最新消息，請稍後再試：{}'.format(str(e))
+
+if __name__ == "__main__":
+    app.run()
