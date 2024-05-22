449d5b89a34b865d24f1839d27c52f00b15564cb
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-05-22 16:22:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, FlexSendMessage, BubbleContainer, BoxComponent, TextComponent, SeparatorComponent
+from linebot.models import MessageEvent, TextMessage, FlexSendMessage, BubbleContainer, BoxComponent, TextComponent, SeparatorComponent
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-# LINE Bot 的 Channel Access Token 和 Channel Secret
 LINE_CHANNEL_ACCESS_TOKEN = "tsGykdGQN1KnwwQWwkkmq7JM0ji0RnYXFa0DBN3sfLVJ4wgcXudGmWpUZst3ZDBHXCL7xp2NhVrR1eDJKdExozjb6DInsSdHeSw1rtrjmz9Bi3Tx/YiI1g4/yGU95a0Jg15MyGM9QFCNdrM2SfU+XQdB04t89/1O/w1cDnyilFU="
 LINE_CHANNEL_SECRET = "0584d0fc476d78024afcd7cbbf8096b4"
 
@@ -44,37 +43,41 @@ def get_transit_info():
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
             
-            # Assuming the transit information is in an element with id="transit-1"
+            # 獲取 id 為 transit-1 的元素
             transit_1_element = soup.find(id="transit-1")
-            time_element = transit_element.find(class_="time display-inline text-frame")
-            transit_1_text = time_element.get_text(strip=True) if time_element else ""
+            transit_1_text = ""
+            if transit_1_element:
+                time_element_1 = transit_1_element.find(class_="time display-inline text-frame")
+                if time_element_1:
+                    transit_1_text = time_element_1.get_text(strip=True)
             
-            # Assuming the second part of transit information is in an element with id="transit-2"
+            # 獲取 id 為 transit-2 的元素
             transit_2_element = soup.find(id="transit-2")
-            time_element = transit_element.find(class_="time display-inline text-frame")
-            transit_2_text = time_element.get_text(strip=True) if time_element else ""
+            transit_2_text = ""
+            if transit_2_element:
+                time_element_2 = transit_2_element.find(class_="time display-inline text-frame")
+                if time_element_2:
+                    transit_2_text = time_element_2.get_text(strip=True)
             
-            # Create a BubbleContainer for FlexMessage
+            # 建立 BubbleContainer 作為 FlexMessage
             transit_bubble = BubbleContainer(
                 body=BoxComponent(
                     layout="vertical",
                     contents=[
-                        TextComponent(text="捷運士林站(中正)-東吳大學", weight="bold", size="x1"),
+                        TextComponent(text="捷運士林站(中正) - 東吳大學", weight="bold", size="xl"),
                         SeparatorComponent(),
-                        TextComponent(text=transit_1_text, wrap=True),
+                        TextComponent(text="557路線: " + transit_1_text, wrap=True),
                         SeparatorComponent(),
-                        TextComponent(text=transit_2_text, wrap=True)
+                        TextComponent(text="300路線: " + transit_2_text, wrap=True)
                     ]
                 )
             )
             return transit_bubble
         else:
-            return "Failed to retrieve the page. Status code: {}".format(response.status_code)
+            return "無法獲取頁面內容。狀態碼: {}".format(response.status_code)
     
     except Exception as e:
         return '無法取得最新消息，請稍後再試：{}'.format(str(e))
 
 if __name__ == "__main__":
     app.run()
-
-https://transit.navitime.com/zh-tw/tw/transfer?start=00016389&goal=00022583
