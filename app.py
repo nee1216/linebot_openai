42a8969bef305c85e38bfccc480ad464ca67866a
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-05-22 17:28:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ def callback():
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
-    body = request.get_data(as_text=True)
+    body = request.get_data(as_text=true)
     app.logger.info("Request body: " + body)
 
     # handle webhook body
