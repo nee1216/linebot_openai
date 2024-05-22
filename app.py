42a4369b7977f95706d67dba427489c8e5f38717
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-05-22 17:25:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,82 +1,16 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, FlexSendMessage, BubbleContainer, BoxComponent, TextComponent, SeparatorComponent
-import requests
-from bs4 import BeautifulSoup
+from linebot.models import MessageEvent, TextMessage, FlexSendMessage
 
 app = Flask(__name__)
 
-LINE_CHANNEL_ACCESS_TOKEN = "tsGykdGQN1KnwwQWwkkmq7JM0ji0RnYXFa0DBN3sfLVJ4wgcXudGmWpUZst3ZDBHXCL7xp2NhVrR1eDJKdExozjb6DInsSdHeSw1rtrjmz9Bi3Tx/YiI1g4/yGU95a0Jg15MyGM9QFCNdrM2SfU+XQdB04t89/1O/w1cDnyilFU="
-LINE_CHANNEL_SECRET = "0584d0fc476d78024afcd7cbbf8096b4"
+# LINE Messaging API credentials
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-@app.route("/")
-def index():
-    return "Hello, World!"
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    if event.message.text == "交通":
-        transit_message = get_transit_info()
-        flex_message = FlexSendMessage(alt_text="交通資訊", contents=transit_message)
-        line_bot_api.reply_message(event.reply_token, flex_message)
-
-def get_transit_info():
-    try:
-        response = requests.get("https://transit.navitime.com/zh-tw/tw/transfer?start=00016389&goal=00022583")
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-
-            # Retrieve and process transit information
-            transit_info = []
-            for transit_id in ["transit-1", "transit-2"]:
-                transit_element = soup.find(id=transit_id)
-                if transit_element:
-                    time_element = transit_element.find(class_="time display-inline text-frame")
-                    if time_element:
-                        transit_info.append(time_element.get_text(strip=True))
-
-            # Create BubbleContainer for FlexMessage
-            transit_bubble = BubbleContainer(
-                body=BoxComponent(
-                    layout="vertical",
-                    contents=[
-                        TextComponent(text="捷運士林站(中正) - 東吳大學", weight="bold", size="md"),
-                        SeparatorComponent(),
-                        TextComponent(text=f"557路線: {transit_info[0] if len(transit_info) > 0 else '無資料'}", wrap=True),
-                        SeparatorComponent(),
-                        TextComponent(text=f"300路線: {transit_info[1] if len(transit_info) > 1 else '無資料'}", wrap=True)
-                    ]
-                )
-            )
-            return transit_bubble
-        else:
-            return "無法獲取頁面內容。狀態碼: {}".format(response.status_code)
-
-    except Exception as e:
-        return '無法取得最新消息，請稍後再試：{}'.format(str(e))
-
-if __name__ == "__main__":
-    app.run()
-
-# FlexMessage JSON template
-flex_message_template = {
+# Flex message JSON template
+flex_message_json = {
   "type": "bubble",
   "body": {
     "type": "box",
@@ -210,3 +144,29 @@ flex_message_template = {
     }
   }
 }
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.message.text == "交通":
+        flex_message = FlexSendMessage(alt_text="公車到站時間", contents=flex_message_json)
+        line_bot_api.reply_message(event.reply_token, flex_message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
