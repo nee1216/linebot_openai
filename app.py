e225c036ec16d23bb32f3d8b10fd036fb69e1025
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-05-29 17:49:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,10 +34,6 @@ def index():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text == "交通":
-        if data == 'action=bus15':
-            get_element_text('https://yunbus.tw/lite/route.php?id=TPE15680', 'https://yunbus.tw/#!stop/TPE54724')
-        elif data == 'action=bus16':
-            get_elements_text('https://yunbus.tw/lite/route.php?id=TPE15681', 'https://yunbus.tw/#!stop/TPE121572')
         # 點選交通按鈕時回覆按鈕模板
         buttons_template = ButtonsTemplate(
             title='請選擇交通資訊',
