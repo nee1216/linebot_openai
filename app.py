d94e6f95754e51120cbf5ddefdea6e96f73619a8
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-05-29 16:37:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 def get_element_text(url, href):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
     }
     response = requests.get(url, headers=headers)
     response.raise_for_status()  # Check if the request was successful
