8e1c3fe1a5c75968df1b3f2f4e08f4a90d6bfdf8
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-05-29 16:23:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import requests
 from bs4 import BeautifulSoup
@@ -42,10 +42,38 @@ def handle_message(event):
 
 def get_transit_info():
     try:
-        # 在此處進行爬蟲或其他方式獲取交通資訊的邏輯
-        return "這裡是交通資訊"
+        element_text = []
+
+        # 第一個網站
+        url = 'https://yunbus.tw/lite/route.php?id=TPE15680'
+        href = 'https://yunbus.tw/#!stop/TPE54724'
+        element_text.append(get_element_text(url, href))
+
+        # 第二個網站
+        url = 'https://yunbus.tw/lite/route.php?id=TPE15681'
+        href = 'https://yunbus.tw/#!stop/TPE121572'
+        element_text.append(get_element_text(url, href))
+
+        return "\n".join(element_text)
+
     except Exception as e:
         return '無法取得最新消息，請稍後再試：{}'.format(str(e))
+
+def get_element_text(url, href):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()  # 確認請求成功
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    element = soup.select_one(f'a[href="{href}"]')
+
+    if element:
+        return f"內科通勤專車: {element.text.strip()}"
+    else:
+        return f'找不到具有 href="{href}" 的元素。'
 
 if __name__ == "__main__":
     app.run()
