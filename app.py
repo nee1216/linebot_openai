ad67010c3bae597f4d83a3363a26d6f4b56a7b95
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-05-29 15:59:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 
-LINE_CHANNEL_ACCESS_TOKEN = "tsGykdGQN1KnwwQWwkkmq7JM0ji0RnYXFa0DBN3sfLVJ4wgcXudGmWpUZst3ZDBHXCL7xp2NhVrR1eDJKdExozjb6DInsSdHeSw1rtrjmz9Bi3Tx/YiI1g4/yGU95a0Jg15MyGM9QFCNdrM2SfU+XQdB04t89/1O/w1cDnyilFU="
-LINE_CHANNEL_SECRET = "0584d0fc476d78024afcd7cbbf8096b4"
-
-
 # LINE Bot credentials
 line_bot_api = LineBotApi('tsGykdGQN1KnwwQWwkkmq7JM0ji0RnYXFa0DBN3sfLVJ4wgcXudGmWpUZst3ZDBHXCL7xp2NhVrR1eDJKdExozjb6DInsSdHeSw1rtrjmz9Bi3Tx/YiI1g4/yGU95a0Jg15MyGM9QFCNdrM2SfU+XQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('0584d0fc476d78024afcd7cbbf8096b4')
