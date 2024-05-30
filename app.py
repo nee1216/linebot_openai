d39fd37a23a70d4e1330f3113d552d5d220812b3
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-05-30 14:46:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def handle_message(event):
 
 def get_element_text(reply_token, url, href):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
     }
     try:
         response = requests.get(url, headers=headers)
