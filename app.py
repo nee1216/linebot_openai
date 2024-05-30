8903f6df873f887bdc3b6d1fa7e7c640e4234314
nee1216 <148304304+nee1216@users.noreply.github.com>
2024-05-30 15:31:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def handle_message(event):
 
 def get_element_text(url, href):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
     }
     response = requests.get(url, headers=headers)
     response.raise_for_status()
